d8e2b9ab12ad62efc0fdfcb8ead00556579ef674
Kyle Runge <krunge1@hotmail.com>
2023-02-03 19:57:00 -0600

    methods from users class

--- bank_branch.py
+++ bank_branch.py
@@ -1,13 +1,11 @@
 import accounts
 import users
 
-kyle = users.User('Kyle','krunge1@hotmail.com','Checking')
+kyle = users.User('Kyle','krunge1@hotmail.com', 'Checking')
 kyleSaving = users.User('Kyle', 'krunge1#@', 'Savings')
 bob = users.User('Bob', 'bob@email', 'Checking')
 
-kyle.account.deposit('Checking', 300).yield_interest('Checking').display_account_info('Checking').withdraw('Checking', 500)
-kyleSaving.account.deposit('Savings', 500).yield_interest('Savings').display_account_info('Savings')
+kyle.makeDeposit('Checking', 300).accountYieldInterest('Checking').displayMyAccount('Checking').makeWithdraw('Checking', 500)
+kyleSaving.makeDeposit('Savings', 500).accountYieldInterest('Savings').displayMyAccount('Savings')
 
-bob.account.deposit('Checking', 5000).withdraw('Checking', 350).deposit('Checking', 5000).transfer_money('Checking', 4000,kyle.account, 'Checking')
-
-kyle.account.transfer_money('Checking', 5000, kyleSaving.account, 'Savings')
+bob.makeDeposit('Checking', 5000).makeWithdraw('Checking', 350).makeDeposit('Checking', 5000)
--- users.py
+++ users.py
@@ -10,3 +10,23 @@ class User:
         self.name = name
         self.email = email
         self.account = accounts.BankAccount(account, int_rate, 0)
+
+    def makeDeposit(self, account_name, amount):
+        accounts.BankAccount.deposit(self.account, account_name, amount)
+        return self
+
+    def makeWithdraw(self, account, amount):
+        accounts.BankAccount.withdraw(self.account, account, amount)
+        return self
+
+    def displayMyAccount(self, account):
+        accounts.BankAccount.display_account_info(self.account, account)
+        return self
+    
+    def accountYieldInterest(self, account):
+        accounts.BankAccount.yield_interest(self.account, account)
+        return self        
+    
+    def makeTransfer(self, amount, other_user, other_user_account):
+        accounts.BankAccount.transfer_money(self.account, amount, other_user, other_user_account)
+
